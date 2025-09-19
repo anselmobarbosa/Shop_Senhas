33d9f24013fd08bc8a9e153a394d8c05b1d53a73
Anselmo Clayton Barbosa <99738391+anselmobarbosa@users.noreply.github.com>
2025-09-19 20:27:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -194,3 +194,13 @@ document.addEventListener('DOMContentLoaded', () => {
     storeList.addEventListener('click', deleteStore);
     searchInput.addEventListener('input', searchStore);
 });
+// Registra o Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('Service Worker registrado com sucesso:', registration);
+    }).catch(error => {
+      console.log('Falha ao registrar o Service Worker:', error);
+    });
+  });
+}
